0bc0cdceb666be041be5fb784539775c82452481
eryx67 <eryx67@gmail.com>
2013-07-09 16:18:17 +0600

    add files

--- src/etsachet.erl
+++ src/etsachet.erl
@@ -1,7 +1,7 @@
 %%% @author Vladimir G. Sekissov <eryx67@gmail.com>
 %%% @copyright (C) 2013, Vladimir G. Sekissov
 %%% @doc
-%%%
+%%% Simple ETS based cache. Inspired by Cadfaerl, git://github.com/ddossot/cadfaerl.git
 %%% @end
 %%% Created :  1 Jul 2013 by Vladimir G. Sekissov <eryx67@gmail.com>
 
@@ -14,7 +14,7 @@
          put/3, put_ttl/4,
          get/2, get/3,
          remove/2,
-         size/1, reset/1
+         cache_size/1, reset/1
         ]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -22,8 +22,13 @@
 -include_lib("stdlib/include/ms_transform.hrl").
 
 -define(DEFAULT_TTL, 356000 * 86400).
--define(STATE_KEY, 'etsachet:state').
--define(EXPIRE_SIZE_THRESHOLD, 100).
+-define(STATE_KEY(CN), {'etsachet:state', CN}).
+
+-ifdef(TEST).
+-define(EXPIRE_SIZE_THRESHOLD, 1).
+-else.
+-define(EXPIRE_SIZE_THRESHOLD, 1000).
+-endif.
 
 -record(cache_state, {name, data_store, expire_store, max_size}).
 -record(state, {cache_state, cleaner_pid}).
@@ -57,12 +62,19 @@ put(CacheName, Key, Value) when is_atom(CacheName) ->
 put_ttl(CacheName, Key, Value, TTL) when is_atom(CacheName),
                                          (TTL == undefined orelse is_integer(TTL)) ->
     State = find_state(CacheName),
-    put_data(Key, Value, expire_at(TTL), State).
+    put_data(Key, Value, expire_at(TTL), State),
+    ok.
 
 %% @doc Get a value, returning undefined if not found.
 -spec get(atom(), term()) -> term() | undefined().
 get(CacheName, Key) when is_atom(CacheName) ->
-    get(CacheName, Key, undefined).
+    Ref = make_ref(),
+    case get(CacheName, Key, Ref) of
+        Ref ->
+            undefined;
+        Val ->
+            {ok, Val}
+    end.
 
 %% @doc Get a value, returning the specified default value if not found.
 -spec get(atom(), term(), term()) -> term().
@@ -72,11 +84,12 @@ get(CacheName, Key, Default) when is_atom(CacheName) ->
 
 remove(CacheName, Key) when is_atom(CacheName) ->
     State = find_state(CacheName),
-    del_data(Key, State).
+    del_data(Key, State),
+    ok.
 
 %% @doc Size of the cache, in number of stored elements.
--spec size(atom()) -> integer().
-size(CacheName) when is_atom(CacheName) ->
+-spec cache_size(atom()) -> integer().
+cache_size(CacheName) when is_atom(CacheName) ->
     State = find_state(CacheName),
     data_size(State).
 
@@ -84,25 +97,26 @@ size(CacheName) when is_atom(CacheName) ->
 -spec reset(atom()) -> ok.
 reset(CacheName) when is_atom(CacheName) ->
     State = find_state(CacheName),
-    data_reset(State).
+    data_reset(State),
+    ok.
 
 %% gen_server callbacks
 init({Name, MaxSize}) ->
     DataTbl = data_table(Name),
-    ClockTbl = clock_table(Name),
+    ExpTbl = expire_table(Name),
     ets:new(DataTbl, [set, named_table, public,
                       {read_concurrency, true},
                       {write_concurrency, true}]),
-    ets:new(ClockTbl, [ordered_set, named_table, public]),
+    ets:new(ExpTbl, [ordered_set, named_table, public]),
 
-    CacheState = #cache_state{name=Name, data_store=DataTbl, expire_store=ClockTbl,
+    CacheState = #cache_state{name=Name, data_store=DataTbl, expire_store=ExpTbl,
                               max_size=MaxSize},
     {ok, #state{cache_state=CacheState}}.
 
 handle_call({expire_size}, _From, S=#state{cleaner_pid=undefined, cache_state=CS}) ->
     #cache_state{max_size=MS} = CS,
     DS = data_size(CS),
-    S1 = if DS + ?EXPIRE_SIZE_THRESHOLD > MS ->
+    S1 = if (DS - ?EXPIRE_SIZE_THRESHOLD) >= MS ->
                  S#state{cleaner_pid=spawn_cleaner(CS)};
             true ->
                  S
@@ -146,10 +160,10 @@ spawn_cleaner(#cache_state{data_store=DS, expire_store=ES}) ->
     Pid.
 
 find_state(CacheName) ->
-    case get(?STATE_KEY) of
+    case get(?STATE_KEY(CacheName)) of
         undefined ->
             {ok, Ret} = gen_server:call(CacheName, {cache_state}),
-            put(?STATE_KEY, Ret),
+            put(?STATE_KEY(CacheName), Ret),
             Ret;
         Ret ->
             Ret
@@ -164,7 +178,9 @@ get_data(Key, Default, _S=#cache_state{data_store=DS,
             ets:delete(DS, Key),
             Default;
         [{_, Val, _}] ->
-            Val
+            Val;
+        [] ->
+            Default
     end.
 
 put_data(Key, Value, ExpireAt, S=#cache_state{name=Name,
@@ -187,8 +203,8 @@ put_data(Key, Value, ExpireAt, S=#cache_state{name=Name,
                 undefined ->
                     ok;
                 _ ->
-                    DSize = data_size(DS),
-                    if DSize >= MS ->
+                    DSize = data_size(S),
+                    if (DSize - ?EXPIRE_SIZE_THRESHOLD) >= MS ->
                             gen_server:call(Name, {expire_size});
                        true ->
                             ok
@@ -220,7 +236,7 @@ data_size(_S=#cache_state{data_store=DS}) ->
 data_table(Name) ->
     list_to_atom(atom_to_list(Name) ++ "_data").
 
-clock_table(Name) ->
+expire_table(Name) ->
     list_to_atom(atom_to_list(Name) ++ "_clock").
 
 expire_at(undefined) ->
@@ -247,28 +263,23 @@ basic_put_get_remove_stats_test() ->
     ?debugMsg("running put/get/remove/stats tests"),
 
     {ok, _Pid} = start_link(basic_cache),
-    ?assertEqual([{miss_count, 0}, {hit_count, 0}], stats(basic_cache)),
-
     ?assertEqual(undefined, get(basic_cache, my_key)),
-    ?assertEqual([{miss_count, 1}, {hit_count, 0}], stats(basic_cache)),
+    ?assertEqual(0, cache_size(basic_cache)),
 
     ?assertEqual(ok, put(basic_cache, my_key, "my_val")),
-    ?assertEqual([{miss_count, 1}, {hit_count, 0}], stats(basic_cache)),
+    ?assertEqual(1, cache_size(basic_cache)),
 
     ?assertEqual({ok, "my_val"}, get(basic_cache, my_key)),
-    ?assertEqual([{miss_count, 1}, {hit_count, 1}], stats(basic_cache)),
 
     ?assertEqual(ok, put(basic_cache, my_key, <<"other_val">>)),
-    ?assertEqual([{miss_count, 1}, {hit_count, 1}], stats(basic_cache)),
+    ?assertEqual(1, cache_size(basic_cache)),
 
     ?assertEqual({ok, <<"other_val">>}, get(basic_cache, my_key)),
-    ?assertEqual([{miss_count, 1}, {hit_count, 2}], stats(basic_cache)),
+    ?assertEqual(1, cache_size(basic_cache)),
 
-    ?assertEqual([my_key], keys(basic_cache)),
-    ?assertEqual(1, size(basic_cache)),
     ?assertEqual(ok, remove(basic_cache, my_key)),
     ?assertEqual(undefined, get(basic_cache, my_key)),
-    ?assertEqual(0, size(basic_cache)),
+    ?assertEqual(0, cache_size(basic_cache)),
     ok = stop(basic_cache).
 
 ttl_put_get_test() ->
@@ -286,7 +297,7 @@ ttl_put_get_test() ->
     ?assertEqual(ok, put(ttl_cache, my_key, "my_val3")),
     timer:sleep(1100),
     ?assertEqual({ok, "my_val3"}, get(ttl_cache, my_key)),
-    ?assertEqual(1, size(ttl_cache)),
+    ?assertEqual(1, cache_size(ttl_cache)),
     ok = stop(ttl_cache).
 
 default_put_get_test() ->
@@ -294,81 +305,53 @@ default_put_get_test() ->
 
     {ok, _Pid} = start_link(default_cache),
     ?assertEqual(undefined, get(default_cache, my_key)),
-    ?assertEqual({ok, 'DEF'}, get(default_cache, my_key, 'DEF')),
-    ?assertEqual(0, size(default_cache)),
+    ?assertEqual('DEF', get(default_cache, my_key, 'DEF')),
+    ?assertEqual(0, cache_size(default_cache)),
     ?assertEqual(ok, put(default_cache, my_key, "my_val")),
-    ?assertEqual({ok, "my_val"}, get(default_cache, my_key, 'DEF')),
+    ?assertEqual("my_val", get(default_cache, my_key, 'DEF')),
 
     ?assertEqual(ok, put_ttl(default_cache, my_key, "my_val2", 1)),
-    ?assertEqual({ok, "my_val2"}, get(default_cache, my_key, 'DEF')),
+    ?assertEqual("my_val2", get(default_cache, my_key, 'DEF')),
     timer:sleep(1100),
-    ?assertEqual({ok, 'DEF'}, get(default_cache, my_key, 'DEF')),
+    ?assertEqual('DEF', get(default_cache, my_key, 'DEF')),
     ok = stop(default_cache).
-
-get_or_fetch_test() ->
-    ?debugMsg("running get_or_fetch() tests"),
-
-    {ok, _Pid} = start_link(fetch_cache),
-    ?assertEqual({ok, 123}, get_or_fetch(fetch_cache, my_key, fun() -> 123 end)),
-    ?assertEqual({ok, 123}, get(fetch_cache, my_key)),
-    ?assertEqual({error, {throw, foo}}, get_or_fetch(fetch_cache, other_key, fun() -> throw(foo) end)),
-    ?assertEqual(undefined, get(fetch_cache, other_key)),
-    ok = stop(fetch_cache).
-
-get_or_fetch_ttl_test() ->
-    ?debugMsg("running get_or_fetch_ttl() tests"),
-
-    {ok, _Pid} = start_link(fetch_ttl_cache),
-    FetchFun = fun() -> now() end,
-    {ok, Val1} = get_or_fetch_ttl(fetch_ttl_cache, my_key, FetchFun, 1),
-    ?assertEqual({ok, Val1}, get_or_fetch_ttl(fetch_ttl_cache, my_key, FetchFun, 1)),
-    timer:sleep(1100),
-    {ok, Val2} = get_or_fetch_ttl(fetch_ttl_cache, my_key, FetchFun, 1),
-    ?assert(Val1 =/= Val2),
-    ok = stop(fetch_ttl_cache).
 
 lru_cache_test() ->
     ?debugMsg("running lru cache tests"),
 
     {ok, _Pid} = start_link(lru_cache, 2),
     ?assertEqual(ok, put(lru_cache, my_key1, "my_val1")),
-    ?assertEqual(1, size(lru_cache)),
+    ?assertEqual(1, cache_size(lru_cache)),
     ?assertEqual(ok, put(lru_cache, my_key2, "my_val2")),
-    ?assertEqual(2, size(lru_cache)),
+    ?assertEqual(2, cache_size(lru_cache)),
     ?assertEqual({ok, "my_val1"}, get(lru_cache, my_key1)),
     ?assertEqual(ok, put(lru_cache, my_key3, "my_val3")),
-    ?assertEqual(2, size(lru_cache)),
-    ?assertEqual({ok, "my_val1"}, get(lru_cache, my_key1)),
-    ?assertEqual(undefined, get(lru_cache, my_key2)),
+    timer:sleep(1000),
+    ?assertEqual(2, cache_size(lru_cache)),
+    ?assertEqual(undefined, get(lru_cache, my_key1)),
+    ?assertEqual({ok, "my_val2"}, get(lru_cache, my_key2)),
     ?assertEqual({ok, "my_val3"}, get(lru_cache, my_key3)),
     ok = stop(lru_cache).
 
 reset_test() ->
     ?debugMsg("running reset() tests"),
 
-    InitialState = initial_state(my_name, 123),
-    ChangedState = InitialState#cache_state{clock=25},
-    ?assertEqual(InitialState, reset_state(ChangedState)),
-
     {ok, _Pid} = start_link(reset_cache),
     ?assertEqual(ok, put(reset_cache, my_key, "my_val")),
     ?assertEqual({ok, "my_val"}, get(reset_cache, my_key)),
-    ?assertEqual(1, size(reset_cache)),
-    ?assertEqual([{miss_count, 0}, {hit_count, 1}], stats(reset_cache)),
+    ?assertEqual(1, cache_size(reset_cache)),
 
     ?assertEqual(ok, reset(reset_cache)),
-
-    ?assertEqual([{miss_count, 0}, {hit_count, 0}], stats(reset_cache)),
     ?assertEqual(undefined, get(reset_cache, my_key)),
-    ?assertEqual(0, size(reset_cache)),
+    ?assertEqual(0, cache_size(reset_cache)),
     ok = stop(reset_cache).
 
 basic_speed_test() ->
     ?debugMsg("running basic speed test"),
-    cadfaerl:start_link(basic_cache),
+    start_link(basic_cache),
     SimplePutGet =
         fun() ->
-                lists:foreach(fun(I)-> cadfaerl:put(basic_cache, I, I), cadfaerl:get(basic_cache, I) end,
+                lists:foreach(fun(I)-> put(basic_cache, I, I), get(basic_cache, I) end,
                               lists:seq(1, ?SPEED_TEST_OP_COUNT))
         end,
     ?debugTime(integer_to_list(?SPEED_TEST_OP_COUNT) ++ " simple put-get", SimplePutGet()),
@@ -376,10 +359,10 @@ basic_speed_test() ->
 
 ttl_put_get_speed_test() ->
     ?debugMsg("running TTL put-get speed test"),
-    cadfaerl:start_link(ttl_cache),
+    etsachet:start_link(ttl_cache),
     TtlPutGet =
         fun() ->
-                lists:foreach(fun(I)-> cadfaerl:put_ttl(ttl_cache, I, I, I rem 10), cadfaerl:get(ttl_cache, I) end,
+                lists:foreach(fun(I)-> etsachet:put_ttl(ttl_cache, I, I, I rem 10), etsachet:get(ttl_cache, I) end,
                               lists:seq(1, ?SPEED_TEST_OP_COUNT))
         end,
     ?debugTime(integer_to_list(?SPEED_TEST_OP_COUNT) ++ " TTL put-get", TtlPutGet()),
@@ -387,10 +370,10 @@ ttl_put_get_speed_test() ->
 
 lru_put_get_speed_test() ->
     ?debugMsg("running LRU put-get speed test"),
-    cadfaerl:start_link(lru_cache, 1000),
+    etsachet:start_link(lru_cache, 1000),
     LruPutGet =
         fun() ->
-                lists:foreach(fun(I)-> cadfaerl:put(lru_cache, I, I), cadfaerl:get(lru_cache, I) end,
+                lists:foreach(fun(I)-> etsachet:put(lru_cache, I, I), etsachet:get(lru_cache, I) end,
                               lists:seq(1, ?SPEED_TEST_OP_COUNT))
         end,
     ?debugTime(integer_to_list(?SPEED_TEST_OP_COUNT) ++ " LRU put-get", LruPutGet()),
@@ -398,10 +381,10 @@ lru_put_get_speed_test() ->
 
 lru_ttl_put_get_speed_test() ->
     ?debugMsg("running LRU TTL put-get speed test"),
-    cadfaerl:start_link(lru_ttl_cache, 1000),
+    etsachet:start_link(lru_ttl_cache, 1000),
     LruTtlPutGet =
         fun() ->
-                lists:foreach(fun(I)-> cadfaerl:put_ttl(lru_ttl_cache, I, I, I rem 10), cadfaerl:get(lru_ttl_cache, I) end,
+                lists:foreach(fun(I)-> etsachet:put_ttl(lru_ttl_cache, I, I, I rem 10), etsachet:get(lru_ttl_cache, I) end,
                               lists:seq(1, ?SPEED_TEST_OP_COUNT))
         end,
     ?debugTime(integer_to_list(?SPEED_TEST_OP_COUNT) ++ " LRU TTL put-get", LruTtlPutGet()),
